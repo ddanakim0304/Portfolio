f53a9bb155f927aedec536f4956716b9032ad6d1
Dain <kim@uni.minerva.edu>
2024-08-12 15:43:02 +0900

    fix: update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ...bundleAnalyzer,
 };
 
 export default nextConfig;
