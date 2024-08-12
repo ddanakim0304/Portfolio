0c1aa590394adb1d7f29f635b5e7e428504fd732
Dain <kim@uni.minerva.edu>
2024-08-12 17:11:42 +0900

    fix: fix bug for dexployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
-// next.config.mjs
-import { withBundleAnalyzer } from '@next/bundle-analyzer';
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  experimental: {
-    appDir: true,
-  },
-  webpack(config) {
-    return config;
-  },
 };
 
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})(nextConfig);
+export default nextConfig;
