e6a814cf80ab0998c90eafaaf3793bb8c9cdfd8b
Dain <kim@uni.minerva.edu>
2024-08-12 15:12:11 +0900

    fix: add space between 'my projects'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,9 +124,8 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
+                  I&apos;ve worked on a variety AI and Game Dev projects. <br></br> Here are a few of my
+                  favorites!
                 </p>
               </div>
             </div>
